e6e23973b19716dff2e950db23f707940e5d0725
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-10-03 18:18:54 +0800

    added: layout for login page

--- frontend/src/pages/register/RegisterPage.tsx
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -1,26 +1,41 @@
 const RegisterPage = () => {
   return (
     <>
-      <div>
-        <span> Register Page </span>
-        <div>
-          <form method="post" >
-            <label htmlFor="username"> 
-                Username
-                <input name="username"/> 
-            </label>
-            <br/>
-            <label htmlFor="password"> 
-                Password
-                <input name="password" type="password"/> 
-            </label>
-            <br/>
-            <button type="submit">Log In</button>
-          </form>
+      <div className="loginPage flex items-center justify-center w-full gap-2 h-screen">
+        <div className="leftPage flex items-center justify-center flex-col">
+          <div className="titleAndLogo flex items-center">
+            <div className="logoPng ">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Grey_Square.svg/480px-Grey_Square.svg.png" className="object-scale-down h-14 w-14"/>
+            </div>
+            <div className="title text-6xl font-bold"> 
+              NurseLink
+            </div>
+          </div>
+          <div className="welcomeTitle text-4xl"> Welcome Back. </div>
+          <div className="containPwUsername float-left" >
+            <form method="post" className="flex items-center flex-col" >
+              <label htmlFor="username"> 
+                  Username
+                  <br/>
+                  <input name="username" id="username" className="bg-slate-200"/> 
+              </label>
+              <br/>
+              <label htmlFor="password"> 
+                  Password
+                  <br/>
+                  <input name="password" type="password" id="password" className="bg-slate-200"/> 
+              </label>
+              <br/>
+              <button type="submit" className="loginBtn bg-slate-400 ">Log In</button>
+            </form>
+          </div>
+          
+          <div>
+            Not a member? Sign up.
+          </div>
         </div>
-        
-        <div>
-          Not a member? Sign up.
+        <div className="rightPage">
+          <img src="https://t4.ftcdn.net/jpg/00/66/01/29/360_F_66012928_ztFfdS8dnLgghWKWxrDOH8FfhrzAkI2Z.jpg"/>
         </div>
       </div>
     </>
