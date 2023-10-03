f0b5e8fbc5a4af10650551643696b83fefea75de
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-10-03 16:58:49 +0800

    added: register page (temporary)

--- frontend/src/pages/register/RegisterPage.tsx
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -1,8 +1,30 @@
-
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
+    <>
+      <div>
+        <span> Register Page </span>
+        <div>
+          <form method="post" >
+            <label htmlFor="username"> 
+                Username
+                <input name="username"/> 
+            </label>
+            <br/>
+            <label htmlFor="password"> 
+                Password
+                <input name="password" type="password"/> 
+            </label>
+            <br/>
+            <button type="submit">Log In</button>
+          </form>
+        </div>
+        
+        <div>
+          Not a member? Sign up.
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RegisterPage
