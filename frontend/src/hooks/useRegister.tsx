46d1b7e86318d268eaf3f041fe905bc15036c046
Johann-Sven-Lee-Uytanlet <114980320+Johann-Sven-Lee-Uytanlet@users.noreply.github.com>
2023-10-14 14:28:49 +0800

    initial stuff

---
+++ frontend/src/hooks/useRegister.tsx
@@ -0,0 +1,11 @@
+import { useRef, useState } from "react"
+import axios from "axios"
+import { Id, toast } from "react-toastify"
+import { useAuth } from "./useAuth"
+import { Action } from "../contexts/AuthContext"
+
+const useRegister = () => {
+}
+
+
+export default useRegister 
