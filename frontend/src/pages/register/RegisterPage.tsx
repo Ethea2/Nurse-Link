688bcdf8983b354500eae27c97046dbd0cc12e51
Nathan Andres <nathandres2001@gmail.com>
2023-10-15 10:50:25 +0800

    added: removed some unused imports

--- frontend/src/pages/register/RegisterPage.tsx
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router"
 import useRegister from "../../hooks/useRegister.tsx"
 
