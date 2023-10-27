8659b319529dd72e6e614c1439a50c89e295146c
Nathan Andres <nathandres2001@gmail.com>
2023-10-28 04:46:25 +0800

    added: removed unused imports

--- frontend/src/components/nurseEdit/EditSide.tsx
+++ frontend/src/components/nurseEdit/EditSide.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom"
-import { useAuth } from "../../hooks/useAuth"
 import UserType from "../../types/userTypes/userType"
 
 const EditSide = ({ user }: { user: UserType }) => {
