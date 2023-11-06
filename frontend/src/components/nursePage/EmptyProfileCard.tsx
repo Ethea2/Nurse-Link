c0f8bd85f5cfa05056b6f0abfa644a9c1f241a19
Johann-Sven-Lee-Uytanlet <114980320+Johann-Sven-Lee-Uytanlet@users.noreply.github.com>
2023-11-06 17:34:07 +0800

    yes

--- frontend/src/components/nursePage/EmptyProfileCard.tsx
+++ frontend/src/components/nursePage/EmptyProfileCard.tsx
@@ -7,23 +7,20 @@ const EmptyProfileCard = ({ nurse }: { nurse: NurseType }) => {
     const { user } = useAuth()
     const nav = useNavigate()
 
-    const [imageLink, setIsAboutVisible] = useState<string>('https://res.cloudinary.com/dialvcsco/image/upload/v1699261841/r1rizqeoij5yify2cut0.jpg')
+    const [imageLink, setImageLink] = useState<string>('https://res.cloudinary.com/dialvcsco/image/upload/v1699261841/r1rizqeoij5yify2cut0.jpg')
 
     
 
     useEffect(() => {
             if(user?.id === nurse?.userId) {
-                    setIsAboutVisible('https://res.cloudinary.com/dialvcsco/image/upload/v1699261843/o9zuezjvog3c2ckipu5r.jpg');
-                }
-
-                
-            
+                setImageLink('https://res.cloudinary.com/dialvcsco/image/upload/v1699261843/o9zuezjvog3c2ckipu5r.jpg');
+            }
             //console.log("Is Searcher:", isSearcher);
         }, [user, nurse]);
 
     return (
         <div>
-            <img src="https://res.cloudinary.com/dialvcsco/image/upload/v1699261841/r1rizqeoij5yify2cut0.jpg"></img>
+            <img src = {imageLink} className = "w-4/5"></img>
         </div>
     )
 }
