e1c5e890ae77d1e712bef02670f91f89cfed98f7
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2023-11-21 11:29:09 +0800

    recommendationsModel.js draft

---
+++ backend/models/recommendationsModel.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const recommendationsSchema = new Schema({
+    recommendations: {
+        given: [{
+            author: {
+                authorID: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true
+                },
+                firstName: {
+                    type: String,
+                    required: true
+                },
+                lastName: {
+                    type: String,
+                    required: true
+                }
+            },
+            receiver: {
+                receiverID: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true
+                },
+                firstName: {
+                    type: String,
+                    required: true
+                },
+                lastName: {
+                    type: String,
+                    required: true
+                }
+            },
+            date: {
+                type: Date,
+                required: true,
+                default: () => Date.now()
+            },
+            description: {
+                type: String,
+                required: true
+            }
+        }],
+        received: [{
+            receiver: {
+                receiverID: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true
+                },
+                firstName: {
+                    type: String,
+                    required: true
+                },
+                lastName: {
+                    type: String,
+                    required: true
+                }
+            },
+            author: {
+                authorID: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true
+                },
+                firstName: {
+                    type: String,
+                    required: true
+                },
+                lastName: {
+                    type: String,
+                    required: true
+                }
+            },
+            date: {
+                type: Date,
+                required: true,
+                default: () => Date.now()
+            },
+            description: {
+                type: String,
+                required: true
+            }
+        }]
+    }
+})
+
+module.exports = mongoose.model("Recommendations", recommendationsSchema, "Recommendations")
