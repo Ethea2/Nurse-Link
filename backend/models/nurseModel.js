3c10de8ea5bee24b64e744f55b802e2c67b2c91e
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2023-11-21 18:29:43 +0800

    added recommendationSchema

--- backend/models/nurseModel.js
+++ backend/models/nurseModel.js
@@ -216,42 +216,12 @@ const nurseSchema = new Schema({
     }],
     recommendations: {
         given: [{
-            authorID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
-            },
-            receiverID: {
-                type: mongoose.Schema.Types.ObjectId,
-                required: true,
-            },
-            date: {
-                type: Date,
-                default: Date.now,
-            },
-            description: {
-                type: String,
-                required: true,
-            }
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Recommendations"
         }],
         received: [{
-            authorID: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
-            },
-            receiverID: {
-                type: mongoose.Schema.Types.ObjectId,
-                required: true,
-            },
-            date: {
-                type: Date,
-                default: Date.now,
-            },
-            description: {
-                type: String,
-                required: true,
-            }
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Recommendations"
         }]
     }
 }, {timestamps: true})
