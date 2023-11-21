adc3c251bdad664b62c17925d948e75ddd1f5dc5
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2023-11-21 19:27:04 +0800

    added useAddRecommendation

--- frontend/src/hooks/useAddRecommendation.tsx
+++ frontend/src/hooks/useAddRecommendation.tsx
@@ -1,9 +1,64 @@
-import { useRef } from "react"
-import { Id, toast } from "react-toastify"
 import axios from "axios"
+import { useRef, useState } from "react"
+import { toast, Id } from "react-toastify"
 
 const useAddRecommendation = () => {
-    // code to successfully post recommendation
-}
+  const toastID = useRef<Id>();
+  const [state, setState] = useState<string>("");
 
-export default useAddRecommendation
+  const addRecommendation = async (
+    userId: string,
+    date: Date,
+    description: string
+  ) => {
+    toastID.current = toast.loading("Adding recommendation...");
+
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_API_URL + `/api/nurse/${userId}/addRecommendation`,
+        {
+          date,
+          description,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      handleSuccess(response.data.message);
+      return response.data;
+    } catch (error) {
+      handleFailure(error);
+      throw error;
+    }
+  };
+
+  const handleSuccess = (message: string) => {
+    setState("success");
+    toast.update(toastID.current ?? "", {
+      render: message,
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      type: "success",
+      isLoading: false,
+    });
+  };
+
+  const handleFailure = (error: any) => {
+    setState("error");
+    console.error("Error adding recommendation:", error);
+    toast.update(toastID.current ?? "", {
+      render: error.response?.data?.message ?? error.message,
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      type: "error",
+      isLoading: false,
+    });
+  };
+
+  return { addRecommendation };
+};
+
+export default useAddRecommendation;
