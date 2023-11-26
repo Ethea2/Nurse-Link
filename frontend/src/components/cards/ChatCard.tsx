357bf30d8dd3a3e7340856e6bb475ce92257c254
isaiahsam <isaiahpascual12@gmail.com>
2023-11-26 23:56:37 +0800

    ChatCard and ChatPage
    connection-based user output
    
    todo: navbar connection plus the modal for the connected user messages

---
+++ frontend/src/components/cards/ChatCard.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { NurseType } from "../../types/nurseTypes/nurseType";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
+// create a modal for the actual chat messages
+
+const ChatCard = ({userId , nurseId }: {userId:string, nurseId: string }) => {
+  // We are currently looking at userId's connection page
+  // userId is connected with nurseId
+  const { data: nurse, loading } = useFetch(`/api/nurse/${nurseId}`);
+
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+
+  const { user } = useAuth()
+  // user is who is currently logged in
+  const nav = useNavigate()
+
+  if (!isVisible) {
+    return null; // Render nothing if the card is not visible
+  }
+
+  return (
+    <div
+      className={`chat-card rounded-3xl p-4 flex items-center justify-left w-100 h-[6rem] ${
+        isHovered ? "hovered" : ""
+      }`}
+      style={{
+        backgroundColor: isHovered ? "#176B87" : "#FFFFFF",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.5)",
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+
+      //add onclick for it to be a button 
+    >
+      <div className="profile-picture w-1/5 rounded-full cursor-pointer">
+        {/* <img className ="pfp" onClick={() => nav(`/nurse/${nurseId}`)} src={nurse?.profilePicture }/> */}
+        <img className ="pfp" src={nurse?.profilePicture }/>
+      </div>
+
+      <div className={`names flex flex-col ${isHovered ? "text-white" : "text-black"}`}
+            style={{fontFamily: "Poppins"}}>
+        <div className="text-xl font-bold">{nurse?.firstName} {nurse?.lastName}</div>
+      </div>
+
+    </div>
+  );
+};
+
+export default ChatCard;
+
+
+
--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -2,16 +2,52 @@ import { Link } from "react-router-dom"
 import { useParams } from "react-router"
 import useFetch from "../../hooks/useFetch"
 import { Suspense } from "react"
+import NurseCard from "../../components/others/NurseCard"
+import ChatCard from "../../components/cards/ChatCard"
+
 
 const ChatPage = () => {
 
     const { userId } = useParams()
-    const { data: nurseId, loading } = useFetch(`/api/nurse/${userId}`)
+    const { data: nurse, loading } = useFetch(`/api/nurse/${userId}`)
 
     return(
-        <div>
-            {nurseId.name}
+
+
+        <>
+        {loading ? (
+            <div>loading...</div>
+        ) : (
+        <div className="flex flex-row md:p-20 gap-20 w-full min-h-screen justify-between">
+                {/* Messages Tab for connected users */}
+                <div>
+                    <p className="font-poppins font-black text-xl">All Messages</p>
+
+            <button>
+                <div className="flex flex-col gap-5 w-50">
+                    {nurse?.connections?.map((connectionId, index) => (
+                        <ChatCard key={index} userId={userId} nurseId={connectionId} />
+                    ))}
+                </div>
+            </button>
+                
+
+                    {/* search function */}
+                </div>
+                
+                {/* Messages Tab */}
+                <div className="flex flex-col main-section w-3/4">
+                    <div className="mb-5">
+                        <p style={{ fontFamily: "Poppins", color: "#053B50", fontSize: "3rem", fontWeight: 900 }}>
+                            {/* {nurse?.connections?.length} Connections */}
+                            Messages
+                        </p>
+                    </div>     
+                </div>
+          
         </div>
+        )}
+    </>
     )
 }
 
